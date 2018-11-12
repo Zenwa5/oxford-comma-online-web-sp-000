7b2b370e6e9bad8680b3a93d4e17d4f7ff76d0be
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-12 20:21:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,11 @@ def oxford_comma(array)
 if array.length == 1
     array.join
   elsif array.length == 2
-  array.join(" and ")
-  last = array.pop
+    array.join(" and ")
+  else
+    last = array.pop
 
-  array << "and #{last}"
-   array.join(", ")
+    array << "and #{last}"
+     array.join(", ")
+   end
 end
